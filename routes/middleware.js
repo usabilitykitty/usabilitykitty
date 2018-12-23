53ddb5f79a189c7fcae800926a002d5fb215fa6b
Mandy Swithenbank <someshysparrow@gmail.com>
2018-12-23 12:04:26 -0500

    Remove contact page, too much spam; make LinkedIn more obvious

--- routes/middleware.js
+++ routes/middleware.js
@@ -23,7 +23,7 @@ exports.initLocals = function (req, res, next) {
 		{ label: 'Blog', key: 'blog', href: '/blog' },
 		//{ label: 'Gallery', key: 'gallery', href: '/gallery' },
 		//{ label: 'Styleguide', key: 'styleguide', href: '/styleguide' },
-		{ label: 'Contact', key: 'contact', href: '/contact' },
+		//{ label: 'Contact', key: 'contact', href: '/contact' },
 	];
 	res.locals.user = req.user;
 	next();
